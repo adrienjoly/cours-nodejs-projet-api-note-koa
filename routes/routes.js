c7599505295589488d938d3ba53b6b1169346cda
CatLeChat <ceaicovschi.ivan@yahoo.com>
2021-04-19 11:30:31 +0200

    /signin all error are now in json body response.

--- routes/routes.js
+++ routes/routes.js
@@ -10,29 +10,26 @@ const jwtKey = globals.JWT_KEY;
 const jwtExpirySeconds = 86400 //24h * 3600(sec dans une heure)
 
 router.post('/signin', async (ctx) => {
+    ctx.type = 'json';
+    ctx.status = 400;
     let req = JSON.stringify(ctx.request.body);
     let res = JSON.parse(req);
-    if(!res.name || !res.password ){
-        ctx.throw(400, 'Il vous manque le login ou le mot de passe' );
+    if(!res.name || !res.password ) {
+        ctx.body = JSON.parse('{"error" : "Il vous manque le login ou le mot de passe"}');
+    }else if(res.password.length < 4){
+        ctx.body = JSON.parse('{"error" : "Le mot de passe doit contenir au moins 4 caractères"}');
+    }else if(res.name.length < 2 || res.name.length > 20) {
+        ctx.body = JSON.parse('{"error" : "Votre identifiant doit contenir entre 2 et 20 caractères"}');
+    }else {
+        ctx.status = 200;
+        const username = res.name;
+        const password = res.password;
+        const token = jwt.sign({username}, jwtKey, {
+            algorithm: "HS256",
+            expiresIn: jwtExpirySeconds,
+        })
+        ctx.body = {token};
     }
-    if(res.password.length < 4) {
-        ctx.throw(400, 'Le mot de passe doit contenir au moins 4 caractères' );
-    }
-    if(res.name.length < 2 || res.name.length > 20) {
-        ctx.throw(400, 'Votre identifiant doit contenir entre 2 et 20 caractères' );
-    }
-    await client.connect(err => {
-        const collection = client.db("notes-api").collection("users");
-        client.close();
-    });
-    // Get credentials from JSON body
-    const username = res.name;
-    const password = res.password;
-    const token = jwt.sign({ username }, jwtKey, {
-        algorithm: "HS256",
-        expiresIn: jwtExpirySeconds,
-    })
-    ctx.body = {token};
 })
 
 router.get('/', async (ctx) => {
