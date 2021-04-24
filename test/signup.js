d6d94d69be4d930e769189f104d932ae1b1090bc
CatLeChat <ceaicovschi.ivan@yahoo.com>
2021-04-24 16:38:01 +0200

    test signup.js

--- test/signup.js
+++ test/signup.js
@@ -2,22 +2,24 @@ const assert = require("assert");
 var chai = require('chai'), chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
-describe("route : /signin", () => {
+describe("route : /signup", () => {
     it("all ok", async () => {
         chai.request("http://localhost:3000")
-            .post('/signin')
+            .post('/signup')
             .send({
-                password: 'carrot',
-                name: 'boby'
+                password: 'test',
+                name: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5)
             })
             .end(function (err, res) {
+                console.log(res.body);
                 assert.strictEqual(res.status, 200);
             });
     });
 
+
     it("password less than 4", async () => {
         chai.request("http://localhost:3000")
-            .post('/signin')
+            .post('/signup')
             .send({
                 password: 'ca',
                 name: 'boby'
@@ -25,12 +27,14 @@ describe("route : /signin", () => {
             .end(function (err, res) {
                 console.log(res.body)
                 assert.strictEqual(res.status, 400);
+                assert.strictEqual(res.body.error, "Le mot de passe doit contenir au moins 4 caractères");
+
             });
     });
 
     it("username contains uppercase", async () => {
         chai.request("http://localhost:3000")
-            .post('/signin')
+            .post('/signup')
             .send({
                 password: 'carrote',
                 name: 'boBy'
@@ -42,16 +46,31 @@ describe("route : /signin", () => {
             });
     });
 
-    it("username contains uppercase", async () => {
+    it("username less 2 and more 20", async () => {
         chai.request("http://localhost:3000")
-            .post('/signin')
+            .post('/signup')
             .send({
                 password: 'carrote',
-                name: 'boBy'
+                name: 'b'
             })
             .end(function (err, res) {
                 console.log(res.body)
                 assert.strictEqual(res.status, 400);
+                assert.strictEqual(res.body.error, "Votre identifiant doit contenir entre 2 et 20 caractères");
+            });
+    });
+
+    it("User exist", async () => {
+        chai.request("http://localhost:3000")
+            .post('/signup')
+            .send({
+                password: 'mama',
+                name: 'test'
+            })
+            .end(function (err, res) {
+                console.log(res.body);
+                assert.strictEqual(res.status, 400);
+                assert.strictEqual(res.body.error, "Cet identifiant est déjà associé à un compte");
             });
     });
 });
