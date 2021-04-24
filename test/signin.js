f9841782dcd2680ab98e835642c7834db17fed64
radhamante <raphael.delassus@efrei.net>
2021-04-24 16:38:39 +0200

    fix : test signin

--- test/signin.js
+++ test/signin.js
@@ -23,8 +23,8 @@ describe("route : /signin", () => {
             name: 'boby'
         })
         .end(function (err, res) {
-            console.log(res.body)
             assert.strictEqual(res.status, 400);
+            assert.strictEqual(res.body.error, "Le mot de passe doit contenir au moins 4 caractères");
         });
     });
 
@@ -36,22 +36,35 @@ describe("route : /signin", () => {
             name: 'boBy'
         })
         .end(function (err, res) {
-            console.log(res.body)
             assert.strictEqual(res.status, 400);
             assert.strictEqual(res.body.error, "Votre identifiant ne doit contenir que des lettres minuscules non accentuées");
         });
     });
 
-    it("username contains uppercase", async () => {
+    it("username contains less than 2 char", async () => {
         chai.request("http://localhost:3000")
         .post('/signin')
         .send({
             password: 'carrote', 
-            name: 'boBy'
+            name: 'b'
+        })
+        .end(function (err, res) {
+            assert.strictEqual(res.status, 400);
+            assert.strictEqual(res.body.error, "Votre identifiant doit contenir entre 2 et 20 caractères");
+        });
+    });
+    
+    it("username is unknown", async () => {
+        chai.request("http://localhost:3000")
+        .post('/signin')
+        .send({
+            password: 'carrote', 
+            name: 'bobbo'
         })
         .end(function (err, res) {
             console.log(res.body)
-            assert.strictEqual(res.status, 400);
+            assert.strictEqual(res.status, 403);
+            assert.strictEqual(res.body.error, "Cet identifiant est inconnu");
         });
     });
 });
